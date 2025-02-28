32ac7528799082de03f3b1c409e4ff2415356472
emiperezdev <emiperez.dev@gmail.com>
2025-02-27 20:35:29 -0600

    another commit

--- src/components/cell-container.tsx
+++
@@ -1,9 +0,0 @@
-interface CellContainerProps {
-  children: React.ReactNode;
-}
-
-export const CellContainer = ({ children }: CellContainerProps) => {
-  return (
-    <div className="flex text-center justify-start">{ children }</div>
-  )
-}
